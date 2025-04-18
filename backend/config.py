b83007cf2675e72440ebd89c24e0ba087a2fcc9e
Agudi2 <113403295+Agudi2@users.noreply.github.com>
2025-04-17 23:05:11 -0400

    Update config.py

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+MONGO_URI = 'mongodb+srv://user1:Midnight@cluster0.3c1j4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
